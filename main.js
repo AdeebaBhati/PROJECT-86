439d09a0adac0645435b4e9aadb6534e0f998a7e
AdeebaBhati <86918907+AdeebaBhati@users.noreply.github.com>
2022-03-11 11:23:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,109 @@
+var canvas=new fabric.Canvas("mycanvas")
+player_x=10
+player_y=10
+block_image_width=30
+block_image_height=30
+var player_object=""
+block_image_object=""
+
+function player(){
+    fabric.Image.fromURL("player.png",function(Img){
+        player_object=Img
+        player_object.scaleToWidth(150)
+        player_object.scaleToHeight(140)
+        player_object.set({
+            left:player_x,
+            top:player_y
+        })
+        canvas.add(player_object)
+    })
+}
+
+function block(get_image){
+    fabric.Image.fromURL(get_image,function(Img){
+        block_image_object=Img
+        block_image_object.scaleToWidth(block_image_width)
+        block_image_object.scaleToHeight(block_image_height)
+        block_image_object.set({
+            left:player_x,
+            top:player_y
+        })
+        canvas.addd(block_image_object)
+    })
+}
+
+window.addEventListener("keydown",keydown)
+
+function keydown(e){
+    keyPressed=e.keyCode
+    if(e.shiftKey ==true && keyPressed=="80"){
+
+        block_image_width=block_image_width+10
+        block_image_height=block_image_height+10
+        document.getElementById("current_width").innerHTML=block_image_width
+        document.getElementById("current_height").innerHTML=block_image_height
+    }
+    if(e.shiftKey && keyPressed=="77"){
+
+        block_image_width=block_image_width-10
+        block_image_height=block_image_height-10
+        document.getElementById("current_width").innerHTML=block_image_width
+        document.getElementById("current_height").innerHTML=block_image_height
+    }
+    if(keyPressed=="38"){
+        up()
+    }
+    if(keyPressed=="40"){
+        down()
+    }
+    if(keyPressed=="37"){
+        left()
+    }
+    if(keyPressed=="39"){
+        right()
+    }
+    if(keyPressed=="80"){
+        block("trunk.jpg")
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+function up(){
+    if(player_y>=0){
+        player_y=player_y-block_image_height
+        canvas.remove(player_object)
+        player()
+    }
+}
+
+function down(){
+    if(player_y<=500){
+        player_y=player_y+block_image_height
+        canvas.remove(player_object)
+        player()
+    }
+}
+
+function left(){
+    if(player_x>0){
+        player_x=player_x-block_image_width
+        canvas.remove(player_object)
+        player()
+    }    
+}
+
+function right(){
+    if(player_x<=850){
+        player_x+block_image_width
+        canvas.remove(player_object)
+        player()
+    }
+}
